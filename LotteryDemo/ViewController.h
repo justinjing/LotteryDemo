5094248662672b38e936ac7e0111b32f4e6ef756
justinjing <jinglijun0612@163.com>
2013-01-31 22:42:46 +0800

    年会抽奖程序

---
+++ LotteryDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LotteryDemo
+//
+//  Created by da zhan on 13-1-31.
+//  Copyright (c) 2013年 da zhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ LotteryDemo/ViewController.m
@@ -0,0 +1,88 @@
+//
+//  ViewController.m
+//  LotteryDemo
+//
+//  Created by da zhan on 13-1-31.
+//  Copyright (c) 2013年 da zhan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController (){
+    UILabel* LotteryLabel;
+    NSMutableArray *luckNumber;
+}
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.view.backgroundColor=[UIColor colorWithPatternImage:[UIImage imageNamed:@"Default.png"]];
+	// Do any additional setup after loading the view, typically from a nib.
+    UIButton *generatorButton=[UIButton buttonWithType:UIButtonTypeRoundedRect];
+    generatorButton.frame=CGRectMake(100, 380,104, 31);
+    [generatorButton  setImage:[UIImage imageNamed:@"button_nomail.png"] forState:UIControlStateHighlighted];
+    [generatorButton  setImage:[UIImage imageNamed:@"button_highlight.png"] forState:UIControlStateNormal];
+    [generatorButton addTarget:self action:@selector(generatorNumber) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:generatorButton];
+    
+    
+    UILabel* generatorLabel=[[UILabel alloc]initWithFrame:CGRectMake(40, 60,120, 40)];
+    generatorLabel.text=@"中将号码:";
+    generatorLabel.font=[UIFont systemFontOfSize:24];
+    generatorLabel.textColor=[UIColor whiteColor];
+    generatorLabel.backgroundColor=[UIColor clearColor];
+    [self.view addSubview:generatorLabel];
+    [generatorLabel release];
+    
+    
+    LotteryLabel=[[UILabel alloc]initWithFrame:CGRectMake(generatorLabel.frame.origin.x+generatorLabel.frame.size.width, 60,200, 40)];
+    LotteryLabel.text=@"0";
+    LotteryLabel.font=[UIFont systemFontOfSize:24];
+    LotteryLabel.textColor=[UIColor whiteColor];
+    LotteryLabel.backgroundColor=[UIColor clearColor];
+    [self.view addSubview:LotteryLabel];
+    [LotteryLabel release];
+    
+    luckNumber=[[NSMutableArray alloc]init];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+ 
+}
+
+-(void)dealloc{
+    [luckNumber release];
+    [super dealloc];
+}
+
+
+-(void)generatorNumber{
+   
+    int a= arc4random()%10000;
+    if ([luckNumber count]==0){
+        
+        [luckNumber addObject:[NSString stringWithFormat:@"%d",a]];
+        LotteryLabel.text=[NSString stringWithFormat:@"%d",(a+1)];
+    }
+    else {
+            for (int i=0; i<[luckNumber count]; i++) {
+            if (a==[[luckNumber objectAtIndex:i] intValue])
+            {
+                [self generatorNumber];
+            }else{
+                [luckNumber addObject:[NSString stringWithFormat:@"%d",a]];
+                LotteryLabel.text=[NSString stringWithFormat:@"%d",(a+1)];
+                return;
+            }
+        }
+    }
+}
+
+@end
